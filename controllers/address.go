bc1fbb0a7be1b27872f8ddcaebca533c838367ae
rocklessg <abdulhafiz.suleiman@decagon.dev>
2024-11-28 20:44:28 +0100

    Update controller

--- controllers/address.go
+++ controllers/address.go
@@ -1,1 +1,22 @@
 package controllers
+
+import(
+
+)
+
+
+func AddAdress() gin.HandlerFunc {
+
+}
+
+func EditHomeAddAdress() gin.HandlerFunc {
+	
+}
+
+func EditWorkAddAdress() gin.HandlerFunc {
+	
+}
+
+func DeleteAddAdress() gin.HandlerFunc {
+	
+}
--- controllers/controllers.go
+++ controllers/controllers.go
@@ -4,11 +4,30 @@ import(
 	
 )
 
+func HashPassword (password string) string{
+	return ""
+}
+
+func VerifyPassword (userPassword string, givenPassword string) (bool, string) {
+
+}
 
 func SingUp() {
 
 }
 
-func Login() {
+func Login() gin.HandlerFunc {
 
+}
+
+func ProductViewerAdmin() gin.HandlerFunc {
+
+}
+
+func SearchProduct() gin.HandlerFunc {
+	
+}
+
+func SearchProductByQuery() gin.HandlerFunc {
+	
 }
